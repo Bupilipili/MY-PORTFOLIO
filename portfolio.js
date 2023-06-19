97a89f1b4cb6bfac2ce9cdaee30767158c0875c0
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-06-19 16:06:24 +0300

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+/*mobile-menu*/
+let menu = document.querySelector('#mobile-menu');
+let burgerbutton = document.querySelector('.hamburgerbutton');
+burgerbutton.addEventListener('click',function(){
+menu.classList.toggle('show-menu')
+
+});
