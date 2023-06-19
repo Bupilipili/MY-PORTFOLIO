d1bb4f703a2b984b4125f2063ab4e1c03c85cb71
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-06-19 16:45:13 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,6 @@ const menu = document.querySelector('#mobileMenu');
 const burgerbutton = document.querySelector('.hamburgerbutton');
 
 burgerbutton.addEventListener('click', () => {
-    menu.classList.toggle('showMenu');
-   
+    /*menu.classList.toggle('showMenu');*/
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
   });
