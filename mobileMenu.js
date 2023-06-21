71121ef3587847e5e04ccf54bd31eb3058e1dbd4
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-06-21 20:39:26 +0300

    Create mobileMenu.js

---
+++ mobileMenu.js
@@ -0,0 +1,19 @@
+const openMenu = document.getElementById('menu');
+const closeMenu = document.getElementById('close_menu');
+const navItems = document.querySelector('.mobile-nav');
+
+openMenu.addEventListener('click', () => {
+  navItems.style.display = 'flex';
+});
+
+closeMenu.addEventListener('click', () => {
+  navItems.style.display = 'none';
+});
+
+navItems.addEventListener('click', () => {
+  navItems.style.display = 'none';
+});
+ 
+
+
+
