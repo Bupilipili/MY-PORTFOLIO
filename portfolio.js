199c6ac047fd9e7f67cb11d68f46b23a30bb08a9
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-06-23 02:42:27 +0300

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,8 +0,0 @@
-/*mobile-menu*/
-const menu = document.querySelector('#mobileMenu');
-const burgerbutton = document.querySelector('.hamburgerbutton');
-
-burgerbutton.addEventListener('click', () => {
-    /*menu.classList.toggle('showMenu');*/
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
-  });
