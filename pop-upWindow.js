ba3c0f2a667465c676ef51c8ffd868bbd0a839f8
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-06-23 00:34:50 +0300

    Update pop-upWindow.js

--- pop-upWindow.js
+++ pop-upWindow.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
  const projectsarray = [
   {
@@ -100,6 +101,112 @@ popButton.forEach((button, buttonIndex) => {
     <li class="item3">${projectsarray[cardIndex].technologies[0]}</li>
     <li class="item3">${projectsarray[cardIndex].technologies[1]}</li>
     <li class="item3">${projectsarray[cardIndex].technologies[2]}</li>
+=======
+const cards = document.querySelector('div');
+const section = document.createElement('div');
+cards.appendChild(section);
+section.className = 'main-container';
+section.innerHTML = `
+    <div class="grid-container">
+    <div class="item1"></div>
+     <div class="flex">
+      <h2 class="item2">Multi-Post Stories Gain+Glory</h2>
+        <ul class="flexbox">
+          <li class="item3">Ruby on Rails</li>
+          <li class="item3">CSS</li>
+          <li class="item3">JavaScript</li>
+          <li class="item3">HTML</li>
+        </ul>
+        <div class="item7">See project</div>
+      </div>
+    </div>
+    <div class="grid-container">
+      <div class="item1"></div>
+       <div class="flex">
+        <h2 class="item2">Multi-Post Stories Gain+Glory</h2>
+        <ul class="flexbox">
+         <li class="item3">Ruby on Rails</li>
+         <li class="item3">CSS</li>
+         <li class="item3">JavaScript</li>
+         <li class="item3">HTML</li>
+       </ul>
+       <div class="item7">See project</div>
+       </div>
+     </div>
+     <div class="grid-container">
+       <div class="item1"></div>
+        <div class="flex">
+         <h2 class="item2">Multi-Post Stories Gain+Glory</h2>
+         <ul class="flexbox">
+           <li class="item3">Ruby on Rails</li>
+           <li class="item3">CSS</li>
+           <li class="item3">JavaScript</li>
+           <li class="item3">HTML</li>
+         </ul>
+         <div class="item7">See project</div>
+       </div>
+     </div>
+     <div class="grid-container">
+       <div class="item1"></div>
+        <div class="flex">
+         <h2 class="item2">Multi-Post Stories Gain+Glory</h2>
+         <ul class="flexbox">
+           <li class="item3">Ruby on Rails</li>
+           <li class="item3">CSS</li>
+           <li class="item3">JavaScript</li>
+           <li class="item3">HTML</li>
+         </ul>
+         <div class="item7">See project</div>
+       </div>
+     </div>
+     <div class="grid-container">
+       <div class="item1"></div>
+        <div class="flex">
+         <h2 class="item2">Multi-Post Stories Gain+Glory</h2>
+         <ul class="flexbox">
+           <li class="item3">Ruby on Rails</li>
+           <li class="item3">CSS</li>
+           <li class="item3">JavaScript</li>
+           <li class="item3">HTML</li>
+         </ul>
+         <div class="item7">See project</div>
+       </div>
+     </div>
+     <div class="grid-container">
+       <div class="item1"></div>
+        <div class="flex">
+         <h2 class="item2">Multi-Post Stories Gain+Glory</h2>
+         <ul class="flexbox">
+           <li class="item3">Ruby on Rails</li>
+           <li class="item3">CSS</li>
+           <li class="item3">JavaScript</li>
+           <li class="item3">HTML</li>
+         </ul>
+        <div class="item7">See project</div>
+       </div>
+     </div>
+`;
+
+
+const open = document.querySelector('.item7');
+const body = document.querySelector('body');
+open.addEventListener('click', ()=> {
+//for the pop-up container
+const main = document.createElement('div');
+main.className = 'Main-pop';
+//for the content of the pop-up container
+const pop = document.createElement('div');
+pop.className = 'pop';
+pop.innerHTML = `<div class="Main-pop">
+<div class="pop">
+<img class="image-pop" src="./features/images/Snapshoot Portfolio.svg">
+<p class="pop-word">Keeping track of hundreds of
+  components</p>
+  <ul class="pop-flexbox">
+    <li class="item3">Ruby on Rails</li>
+    <li class="item3">CSS</li>
+    <li class="item3">JavaScript</li>
+>>>>>>> parent of 3c61cf0 (Add function to the pop-upWindow.js)
   </ul>
  <p class="lorem">${projectsarray[cardIndex].description[0]}</p>
   <p class="lorem">${projectsarray[cardIndex].description[0]}</p>
@@ -123,6 +230,7 @@ popButton.forEach((button, buttonIndex) => {
 <li class="item3x">${projectsarray[cardIndex].Keycomponents[5]}</li>
 </ul>
 </div>
+<<<<<<< HEAD
 <div class="button-spacingx">
 <a href="${projectsarray[cardIndex].liveVersion}" class="item8x"><p>See live</p><img src="${projectsarray[cardIndex].featuredimages[2]}"></a>
 <a href="${projectsarray[cardIndex].sourceLink}" class="item8x"><p>See source</p><img src="${projectsarray[cardIndex].featuredimages[3]}"></a>
@@ -145,4 +253,16 @@ popButton.forEach((button, buttonIndex) => {
       document.querySelector('body').style.overflow = 'scroll';
     });
   });
+=======
+`;
+main.appendChild(pop);
+body.appendChild(main)
+>>>>>>> parent of 3c61cf0 (Add function to the pop-upWindow.js)
 });
+
+
+
+
+
+
+
