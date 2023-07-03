adb920628a9a0d0f940bbbd9cc6643d5bcea1676
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-03 17:51:44 +0300

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,11 +1,11 @@
 const form = document.querySelector('.form');
 const inputEmail = document.querySelector('.email-input');
 const errorMessage = document.querySelector('.error-message');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+form.addEventListener('submit', function(event) {
   if (inputEmail.value === inputEmail.value.toLowerCase()) {
     form.submit();
   } else {
+    event.preventDefault();
     errorMessage.style.display = 'block';
   }
 });
